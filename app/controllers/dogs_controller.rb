8c93c763c2a1ada03241399710074938851b8ba6
Eric Kim <hyeokjungkim@gmail.com>
2019-06-13 14:04:39 -0400

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+  # before_action :find_dog, only: [:show, :edit, :update, :delete]
+  
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  # def find_dog
+  #   @dog = Dog.find(params[:id])
+  # end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,11 @@
 class Employee < ApplicationRecord
+  belongs_to :dog
+
+  validates :title, uniqueness: true, presence: true
+  validates :alias, uniqueness: true, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
 end
